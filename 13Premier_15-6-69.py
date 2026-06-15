a0ff70c1656344889791eca242c6d21697a69d70
26940-premier <26940@bbw.ac.th>
2026-06-15 09:44:41 +0700

    ทำโปรแกรมนับเลขว่าเป็นเลขคู่หรือเลขคี่
    15/6/69 เรียนการเขียนทำโปรแกรมนับเลขคู่เลขคี่

---
+++ 13Premier_15-6-69.py
@@ -0,0 +1,12 @@
+print("Program name : Even or Odd?")
+
+number = int(input(" Enter your number: "))
+
+if number %2 == 0 :
+    print("Even Numbers")
+else:
+    print("Odd Numbers")
+
+print("By Premier 4/4")
+
+print("Thank you")
