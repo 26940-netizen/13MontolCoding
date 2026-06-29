a9b150d31aa8b1789ab68ceb521a689d204063d4
26940-premier <26940@bbw.ac.th>
2026-06-29 10:12:15 +0700

    โปรแกรมคำนวณค่าใช้จ่าย 18/06/69

---
+++ premier13_18-06-69-02.py
@@ -0,0 +1,10 @@
+print("โปรแกรมคำนวณค่าใช่จ่าย")
+price1 = int(input("ราคาสินค้า1: "))
+price2 = int(input("ราคาสินค้า2: "))
+money = 120
+total = price1 + price2
+
+change = money - total
+
+print("จำนวนเงินทอน\n")
+print(change)
